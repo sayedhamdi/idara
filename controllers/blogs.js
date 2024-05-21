b1ea1f8aa0a5b6fee20d9b0a7244d886a827314e
Sayed Hamdi <32652375+sayedhamdi@users.noreply.github.com>
2024-05-21 16:12:58 +0200

    Add files via upload

---
+++ controllers/blogs.js
@@ -0,0 +1,103 @@
+let blogs = [
+    {
+      id: 1,
+      title: "How to createt web server",
+      createdAt: "20-05-2024",
+      author: "Ahmed moshen",
+      image : "https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
+      content:"you should look at the documentation"
+    },
+    {
+      id: 2,
+      title: "How to createt react app",
+      createdAt: "20-05-2024",
+      author: "Ahmed moshen",
+      image : "https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
+      content:"you should look at the documentation"
+    },
+  ];
+  
+
+
+
+const createBlog = (req,res)=>{
+    // verify that data passes through
+    // validation
+    for (const key of Object.keys(req.body)){
+        if(req.body[key] ==""){
+            return res.status(400).json({message:"verify blog content one or more elements are empty"})
+        }
+    }
+    let newBlog = {id:blogs.length +1 , ...req.body}
+    blogs.push(newBlog);
+    res.status(201).json(newBlog)
+}
+
+
+const getAllBlogs = (req,res)=>{
+  res.status(200).json(blogs);
+}
+
+
+const getBlogById = (req,res)=>{
+    let blogId = req.params.id
+
+    let blog = blogs.find((b=>b.id == blogId))
+    if(!blog){
+        return res.status(400).json({message:"No blog with id "+blogId})
+    }
+    res.status(200).json(blog);
+}
+
+const updateBlogById = (req,res)=>{
+    // verification de l'existence de l'element 
+    const blogId = req.params.id;
+
+    const blogIndex = blogs.findIndex(b =>b.id ==blogId);
+    if (!blogIndex){
+        return res.status(404).json({message: "Blog not found wrong id"});
+    }
+
+    // verification mtaa el body 
+    for (const key of Object.keys(req.body)){
+        if(req.body[key] ==""){
+            return res.status(400).json({message:"verify blog content one or more elements are empty"})
+        }
+    }
+
+
+    // update  
+    blogs[blogIndex] = {id: blogs[blogIndex].id,...req.body}
+
+    res.json(blogs[blogIndex]);
+
+    
+
+    // verification des champs
+    
+}
+
+const deleteBlogById = (req,res)=>{
+    let blogId = req.params.id;
+    let blogIndex = blogs.findIndex(b=>b.id==blogId);
+    if(!blogIndex){
+        return res.status(404).json({message:"Blog not found !"})
+    }
+    let blogtoDelete = blogs[blogIndex]
+
+
+    blogs.splice(blogIndex,1);
+    
+    res.json(blogtoDelete)
+
+}
+
+
+
+module.exports = {
+    getAllBlogs,
+    getBlogById,
+    createBlog,
+    deleteBlogById,
+    updateBlogById
+}
--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 const express = require("express");
 const morgan = require("morgan");
+const blogsRouter = require("./routes/blogs")
+
+
+
 const app = express();
+
+
+//definition des middlewares
 app.use(express.json())
+app.use(morgan("dev"));
 
-app.use(morgan("dev"))
-const blogs = [
-  {
-    id: 1,
-    title: "How to createt web server",
-    createdAt: "20-05-2024",
-    author: "Ahmed moshen",
-    image : "https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    content:"you should look at the documentation"
-  },
-  {
-    id: 2,
-    title: "How to createt react app",
-    createdAt: "20-05-2024",
-    author: "Ahmed moshen",
-    image : "https://miro.medium.com/v2/resize:fit:1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    content:"you should look at the documentation"
-  },
-];
 
-app.get("/", (req, res) => {
-  res.json({ hello: "you are welcome" });
-});
+
+//defintion des routes
+app.use("/blogs",blogsRouter)
+
+
+
+
 // get all blogs
-app.get("/blogs", (req, res) => {
-  res.status(200).json(blogs);
-});
-// get single blog by id
-app.get("/blogs/:id", (req, res) => {
-    let blogId = req.params.id
-
-    let blog = blogs.find((b=>b.id == blogId))
-    if(!blog){
-        return res.status(400).json({message:"No blog with id "+blogId})
+/*function verifyAdmin(req,res,next){
+    if (req.query.username =="admin" && req.query.password=="1234"){
+        next()
+    }else{
+        res.status(403).json({message:"not authorised to do this operation"})
     }
-    res.status(200).json(blog);
-  });
+}*/
+
+/*app.get("/blogs" ,verifyAdmin,(req, res) => {
+  res.status(200).json(blogs);
+});*/
+// get single blog by id
 
 
 
-app.post("/blogs",(req,res)=>{
-    // traitement pour créer un blog
-    console.log(req.body)
-    res.json({message:"wsel el jweb"})
-});
+
+
 
 app.listen(8000, () => {
   console.log("listening on port 8000 ! ");
