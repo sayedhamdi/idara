88fbc52826d2865a085234ab1ad276a468f91253
Sayed Hamdi <32652375+sayedhamdi@users.noreply.github.com>
2024-05-22 23:14:57 +0200

    Add files via upload

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,15 @@
 const blogSchema = require("../models/Blogs")
-  
+const blogValidation = require("../validation/Blogs")  
 
 
 
 const createBlog = async (req,res)=>{
     // verify that data passes through
     // validation
-    for (const key of Object.keys(req.body)){
-        if(req.body[key] ==""){
-            return res.status(400).json({message:"verify blog content one or more elements are empty"})
-        }
+
+    let {error,value } = blogValidation.validate(req.body);
+    if (error){
+        return res.status(400).json({message : error.details[0].message})
     }
     try{
         let newBlog = await blogSchema.create(req.body)
@@ -34,56 +34,45 @@ const getAllBlogs = async (req,res)=>{
 }
 
 
-const getBlogById = (req,res)=>{
+const getBlogById = async (req,res)=>{
     let blogId = req.params.id
-
-    let blog = blogs.find((b=>b.id == blogId))
-    if(!blog){
+    
+    try  {
+        let blog = await blogSchema.findById(blogId)
+        res.json(blog)
+    }catch(e){
         return res.status(400).json({message:"No blog with id "+blogId})
     }
-    res.status(200).json(blog);
+
 }
 
-const updateBlogById = (req,res)=>{
+const updateBlogById = async (req,res)=>{
     // verification de l'existence de l'element 
     const blogId = req.params.id;
 
-    const blogIndex = blogs.findIndex(b =>b.id ==blogId);
-    if (!blogIndex){
+    // validate data
+    let {error,value } = blogValidation.validate(req.body);
+    if (error){
+        return res.status(400).json({message : error.details[0].message})
+    }
+    // find and update
+    try  {
+        console.log(value);
+        const blog = await  blogSchema.findByIdAndUpdate(blogId,value,{new:true})
+        res.json(blog)
+    }catch(e){
         return res.status(404).json({message: "Blog not found wrong id"});
     }
-
-    // verification mtaa el body 
-    for (const key of Object.keys(req.body)){
-        if(req.body[key] ==""){
-            return res.status(400).json({message:"verify blog content one or more elements are empty"})
-        }
-    }
-
-
-    // update  
-    blogs[blogIndex] = {id: blogs[blogIndex].id,...req.body}
-
-    res.json(blogs[blogIndex]);
-
-    
-
-    // verification des champs
-    
 }
 
-const deleteBlogById = (req,res)=>{
+const deleteBlogById = async (req,res)=>{
     let blogId = req.params.id;
-    let blogIndex = blogs.findIndex(b=>b.id==blogId);
-    if(!blogIndex){
-        return res.status(404).json({message:"Blog not found !"})
+    try{
+        let blog = await blogSchema.findByIdAndDelete(blogId)
+        res.json({message:"deleted element with id " + blogId})
+    }catch(e){
+        res.status(404).json({message:"no  blog with id "+blogId})
     }
-    let blogtoDelete = blogs[blogIndex]
-
-
-    blogs.splice(blogIndex,1);
-
-    res.json(blogtoDelete)
 
 }
 
